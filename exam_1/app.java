81a5b5b0dcf140ea199e4294a08d568f9a188e4f
Allen Siaton <111488480+astar0thhh@users.noreply.github.com>
2022-09-08 14:48:30 +0800

    Create app.java

---
+++ exam_1/app.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+
+        Students Allen = new Students("Siaton", "Allen", 'M', "05/30/2004", 2022104614, "siatonam@students.national-u.edu.ph", true);
+        Allen.studentInfo();
+        Allen.sayMyStudentNumber();
+        Allen.sayMyEmailAddress();
+        Allen.amIAwesome();
+
+       
+    }
+}
